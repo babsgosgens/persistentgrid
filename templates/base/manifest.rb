c1da4283c6a52c356b68f1a1c0f515bc30edd554
Babs Gosgens <babsgosgens@gmail.com>
2013-08-14 14:48:56 +0200

    Oops. Forgot to add the new folders.

---
+++ templates/base/manifest.rb
@@ -0,0 +1,17 @@
+stylesheet 'persistentgrid.scss', :media => "screen, projection"
+
+description "A solid base for your projects. Includes the grid and base styles."
+
+help %Q{
+Please see the Persistent Grid website for all documentation and tutorials:
+
+    http://www.persistentgrid.com
+}
+
+welcome_message %Q{
+Please see the Persistent Grid website for all documentation and tutorials:
+
+    http://www.persistentgrid.com
+
+To get started, set up your grid in the base partial by following the inline instructions.
+}
