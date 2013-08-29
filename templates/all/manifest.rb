aebb0d3b1e67f5b4d8b1bc5753436e953e3a4de2
Babs Gosgens <babsgosgens@gmail.com>
2013-08-29 21:20:44 +0200

    Move elements to helpers

--- templates/all/manifest.rb
+++ templates/all/manifest.rb
@@ -1,6 +1,6 @@
 stylesheet 'persistentgrid.scss', :media => "screen, projection"
 
-description "A solid base for your projects: Persistent Grid in all its glory. Includes the base styles, the layout system and the modules."
+description "A solid base for your projects: Persistent Grid in all its glory. Includes the base styles, the layout system and the modules ."
 
 help %Q{
 Please see the Persistent Grid website for all documentation and tutorials:
